f213432dc59f910a41753ca8c443ade619524c06
DeepakGupta4 <dkguptak9369.com>
2025-01-14 14:35:30 +0530

    Day 4th

--- src/components/Addmembers/Addmembers.js
+++ src/components/Addmembers/Addmembers.js
@@ -1,39 +1,121 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import Stack from "@mui/material/Stack";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const Addmembers = () => {
-   const [inputField, setInputField] = useState({name:"", mobileNo:"", address:"", profilePic:"", joiningDate:""});
-  
-    const handleOnchange = (event,name)=>{
-      setInputField({...inputField,[name]:event.target.value})
-    }
-    console.log(inputField)
+  const [inputField, setInputField] = useState({
+    name: "",
+    mobileNo: "",
+    address: "",
+    profilePic: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShuqugb_wHBWBQZvkLM2wByTZ5uoVspswJ7z0KoqOupYq0W1h4evPU2LGrd1b0pf5y95g&usqp=CAU",
+    joiningDate: "",
+  });
 
+  const [loaderImage, setLoaderImage] = useState(false)
+
+  const handleOnchange = (event, name) => {
+    setInputField({ ...inputField, [name]: event.target.value });
+  };
+  console.log(inputField);
+
+  const uploadImage = async (event) => {
+    setLoaderImage(true)
+    console.log("image uploading..");
+    const files = event.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+
+    // dyhn1jppi
+
+    data.append("upload_preset", "gym-management");
+
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/dyhn1jppi/image/upload",
+        data
+      );
+      console.log(response);
+      const imageUrl = response.data.url;
+      setInputField({ ...inputField, ["profilePic"]: imageUrl });
+      setLoaderImage(false)
+    } catch (error) {
+      console.log(error);
+      setLoaderImage(false)
+    }
+  };
 
   return (
-    <div className='text-black'>
-        <div className='grid gap-5 grid-cols-2 text-lg'>
+    <div className="text-black">
+      <div className="grid gap-5 grid-cols-2 text-lg">
+        <input
+          value={inputField.name}
+          onChange={(event) => {
+            handleOnchange(event, "name");
+          }}
+          placeholder="Name of the Joinee"
+          type="text"
+          className="border-2 w-[90%] pl-3 pr-3 pt-2 pb-2 border-slate-400 rounded-md h-12"
+        />
+        <input
+          value={inputField.mobileNo}
+          onChange={(event) => {
+            handleOnchange(event, "mobileNo");
+          }}
+          placeholder="Mobile No"
+          type="text"
+          className="border-2 w-[90%] pl-3 pr-3 pt-2 pb-2 border-slate-400 rounded-md h-12"
+        />
+        <input
+          value={inputField.address}
+          onChange={(event) => {
+            handleOnchange(event, "address");
+          }}
+          placeholder="Enter Address"
+          type="text"
+          className="border-2 w-[90%] pl-3 pr-3 pt-2 pb-2 border-slate-400 rounded-md h-12"
+        />
+        <input
+          value={inputField.joiningDate}
+          onChange={(event) => {
+            handleOnchange(event, "joiningDate");
+          }}
+          type="date"
+          className="border-2 w-[90%] pl-3 pr-3 pt-2 pb-2 border-slate-400 rounded-md h-12"
+        />
 
-          <input value={inputField.name} onChange={(event)=>{handleOnchange(event,"name")}} placeholder='Name of the Joinee' type='text' className='border-2 w-[90%] pl-3 pr-3 pt-2 pb-2 border-slate-400 rounded-md h-12' />
-          <input value={inputField.mobileNo} onChange={(event)=>{handleOnchange(event,"mobileNo")}} placeholder='Mobile No' type='text' className='border-2 w-[90%] pl-3 pr-3 pt-2 pb-2 border-slate-400 rounded-md h-12' />
-          <input value={inputField.address} onChange={(event)=>{handleOnchange(event,"address")}} placeholder='Enter Address' type='text' className='border-2 w-[90%] pl-3 pr-3 pt-2 pb-2 border-slate-400 rounded-md h-12' />
-          <input value={inputField.joiningDate} onChange={(event)=>{handleOnchange(event,"joiningDate")}} type='date' className='border-2 w-[90%] pl-3 pr-3 pt-2 pb-2 border-slate-400 rounded-md h-12' />
+        {/* drop down */}
 
-          {/* drop down */}
+        <select className="border-2 w-[90%] h-12 pt-2 pb-2 border-slate-400 rounded-md placeholder:text-gray">
+          <option>1 Month Membership</option>
+          <option>2 Month Membership</option>
+          <option>3 Month Membership</option>
+        </select>
 
-          <select className='border-2 w-[90%] h-12 pt-2 pb-2 border-slate-400 rounded-md placeholder:text-gray'>
-            <option>1 Month Membership</option>
-            <option>2 Month Membership</option>
-            <option>3 Month Membership</option>
-          </select>
+        <input
+          type="file"
+          onChange={(e) => {
+            uploadImage(e);
+          }}
+        />
 
-          <input type='file' />
-          <div className='w-1/4'>
-              <img src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShuqugb_wHBWBQZvkLM2wByTZ5uoVspswJ7z0KoqOupYq0W1h4evPU2LGrd1b0pf5y95g&usqp=CAU"} className='w-full h-full rounded-full' />
-          </div>
-          <div className='p-3 border-2 w-28 text-lg h-14 text-center bg-slate-900 text-white rounded-xl cursor-pointer hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500'>Register</div>
+        <div className="w-[100px] h-[100px]">
+          <img
+            src={inputField.profilePic}
+            className="w-full h-full rounded-full"
+          />
+          {loaderImage && (
+        <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
+          <LinearProgress color="secondary" />
+        </Stack>
+      )}
         </div>
+        <div className="p-3 border-2 w-28 text-lg h-14 text-center bg-slate-900 text-white rounded-xl cursor-pointer hover:bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+          Register
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Addmembers
+export default Addmembers;
--- src/components/signup/Signup.js
+++ src/components/signup/Signup.js
@@ -1,46 +1,140 @@
-import React, { useState } from 'react'
-import './signup.css'
-import Modal from '../modal/Modal'
-import ForotPassword from '../forgotpassword/ForotPassword'
+import React, { useState } from "react";
+import "./signup.css";
+import Modal from "../modal/Modal";
+import ForotPassword from "../forgotpassword/ForotPassword";
+import axios from "axios";
+import Stack from "@mui/material/Stack";
+import LinearProgress from "@mui/material/LinearProgress";
 
 const Signup = () => {
+  const [forgotPassword, setForgotPassword] = useState(false);
+  const [inputField, setInputField] = useState({
+    gymName: "",
+    email: "",
+    userName: "",
+    password: "",
+    profilePic:
+      "https://c4.wallpaperflare.com/wallpaper/199/924/33/muscle-muscle-bodybuilding-press-wallpaper-preview.jpg",
+  });
 
-    const [forgotPassword, setForgotPassword] = useState(false)
-    const [inputField, setInputField] = useState({gymName:"",email:"",userName:"",password:"",profilePic:"https://c4.wallpaperflare.com/wallpaper/199/924/33/muscle-muscle-bodybuilding-press-wallpaper-preview.jpg"})
+  const [loaderImage, setLoaderImage] = useState(false);
 
-    const handleClose = ()=>{
-        setForgotPassword(prev=>!prev)
+  const handleClose = () => {
+    setForgotPassword((prev) => !prev);
+  };
+
+  const handleOnchange = (event, name) => {
+    setInputField({ ...inputField, [name]: event.target.value });
+  };
+  console.log(inputField);
+
+  const uploadImage = async (event) => {
+    setLoaderImage(true);
+    console.log("image uploading..");
+    const files = event.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+
+    // dyhn1jppi
+
+    data.append("upload_preset", "gym-management");
+
+    try {
+      const response = await axios.post(
+        "https://api.cloudinary.com/v1_1/dyhn1jppi/image/upload",
+        data
+      );
+      console.log(response);
+      const imageUrl = response.data.url;
+      setLoaderImage(false)
+      setInputField({ ...inputField, ["profilePic"]: imageUrl });
+    } catch (error) {
+      console.log(error);
+      setLoaderImage(false)
     }
-
-
-    const handleOnchange = (event,name) =>{
-      setInputField({...inputField,[name]:event.target.value})
-    }
-    console.log(inputField)
-
-    
+  };
 
   return (
-    
-        <div className='customSinup w-1/3 p-10 mt-20 ml-20 bg-gray-100 bg-opacity-50 h-[450px] overflow-y-auto'>
-           <div className='font-semibold text-gray-600 text-center text-3xl'>Register Your Gym</div>
+    <div className="customSinup w-1/3 p-10 mt-20 ml-20 bg-gray-100 bg-opacity-50 h-[450px] overflow-y-auto">
+      <div className="font-semibold text-gray-600 text-center text-3xl">
+        Register Your Gym
+      </div>
 
-           <input value={inputField.email} onChange={(event)=>{handleOnchange(event,"email")}} type='text' className='w-full my-10 p-2 rounded-lg' placeholder='Enter email' />
+      <input
+        value={inputField.email}
+        onChange={(event) => {
+          handleOnchange(event, "email");
+        }}
+        type="text"
+        className="w-full my-10 p-2 rounded-lg"
+        placeholder="Enter email"
+      />
 
-           <input value={inputField.gymName} onChange={(event)=>{handleOnchange(event,"gymName")}} type='text' className='w-full mb-10 p-2 rounded-lg' placeholder='Enter Gymname' />
+      <input
+        value={inputField.gymName}
+        onChange={(event) => {
+          handleOnchange(event, "gymName");
+        }}
+        type="text"
+        className="w-full mb-10 p-2 rounded-lg"
+        placeholder="Enter Gymname"
+      />
 
-           <input value={inputField.userName} onChange={(event)=>{handleOnchange(event,"userName")}} type='text' className='w-full mb-10 p-2 rounded-lg' placeholder='Enter Username' />
+      <input
+        value={inputField.userName}
+        onChange={(event) => {
+          handleOnchange(event, "userName");
+        }}
+        type="text"
+        className="w-full mb-10 p-2 rounded-lg"
+        placeholder="Enter Username"
+      />
 
-           <input value={inputField.password} onChange={(event)=>{handleOnchange(event,"password")}} type='password' className='w-full mb-10 p-2 rounded-lg' placeholder='Enter password' />
+      <input
+        value={inputField.password}
+        onChange={(event) => {
+          handleOnchange(event, "password");
+        }}
+        type="password"
+        className="w-full mb-10 p-2 rounded-lg"
+        placeholder="Enter password"
+      />
 
-           <input type='file' className='w-full mb-5 p-2 rounded-lg' />
-           <img src={inputField.profilePic} alt="" className='h-[180px] w-[200px] mb-8' />
-           <div className='p-2 w-[80%] border-2 bg-slate-800 mx-auto rounded-lg text-white text-center text-lg hover:bg-white hover:text-black font-semibold cursor-pointer'>Register</div>
-           <div className='p-2 w-[80%] mt-5 border-2 bg-slate-800 mx-auto rounded-lg text-white text-center text-lg hover:bg-white hover:text-black font-semibold cursor-pointer' onClick={()=>handleClose()}>Forget Password</div>
-           {forgotPassword && <Modal header="Forgot Password" handleClose={handleClose} content={<ForotPassword />} />}
-           </div>
-    
-  )
-}
+      <input
+        type="file"
+        onChange={(e) => {
+          uploadImage(e);
+        }}
+        className="w-full mb-5 p-2 rounded-lg"
+      />
+      {loaderImage && (
+        <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
+          <LinearProgress color="secondary" />
+        </Stack>
+      )}
+      <img
+        src={inputField.profilePic}
+        alt=""
+        className="h-[180px] w-[200px] mb-8"
+      />
+      <div className="p-2 w-[80%] border-2 bg-slate-800 mx-auto rounded-lg text-white text-center text-lg hover:bg-white hover:text-black font-semibold cursor-pointer">
+        Register
+      </div>
+      <div
+        className="p-2 w-[80%] mt-5 border-2 bg-slate-800 mx-auto rounded-lg text-white text-center text-lg hover:bg-white hover:text-black font-semibold cursor-pointer"
+        onClick={() => handleClose()}
+      >
+        Forget Password
+      </div>
+      {forgotPassword && (
+        <Modal
+          header="Forgot Password"
+          handleClose={handleClose}
+          content={<ForotPassword />}
+        />
+      )}
+    </div>
+  );
+};
 
-export default Signup
+export default Signup;
